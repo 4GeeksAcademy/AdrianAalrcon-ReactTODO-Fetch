348c968611a7168e7c89269e47c836bd709a3a0f
COCOASOO <85440064+COCOASOO@users.noreply.github.com>
2024-04-21 17:27:42 +0000

    lista usuarios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TaskItem from "./taskitem";
-
+import UserName from "./username";
 
 const Home = () => {
     const [taskInput, setTaskInput] = useState("");
     const [tasks, setTasks] = useState([]);
 
+    const [users, setUsers] = useState([]);
+    const [newUser, setNewUser] = useState('');
+
+
+    const getAllUsers = async () => {
+        try {
+            const response = await fetch('https://playground.4geeks.com/todo/users/');
+            if (!response.ok) {
+                throw new Error(`Error fetching users: ${response.statusText}`);
+            }
+
+            const data = await response.json();
+            console.log("Datos obtenidos:", data);
+
+            if (data.hasOwnProperty('users') && Array.isArray(data.users)) {
+                setUsers(data.users);
+            } else {
+                console.error("La propiedad 'users' no es un array o no existe.");
+            }
+
+        } catch (error) {
+            console.error("Error al obtener usuarios:", error);
+        }
+    };
+
+    // Execute at the beggining
+    useEffect(() => {
+        getAllUsers()
+    }, [])
+
+    // Create new user
+    const createNewUser = async () => {
+        if (newUser) {
+            await fetch(`https://playground.4geeks.com/todo/users/${newUser}`, {
+                method: 'POST'
+            }).then(resp => {
+                if (resp.ok) {
+                    setNewUser('');
+                    getAllUsers();
+                } else {
+                    alert('Error whle creating new user')
+                }
+            });
+        }
+    }
+    // ........................
     const handleAddTask = () => {
         if (taskInput.trim() !== "") {
             setTasks([...tasks, taskInput]);
@@ -23,9 +69,31 @@ const Home = () => {
         }
     };
 
+    console.log(users);
     return (
         <div className="container mt-5">
             <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <div className="card shadow">
+                        <div className="card-body">
+                            <div className="input-group mb-3">
+                                <input placeholder="New user name ..." className="form-control" value={newUser} onChange={(e) => setNewUser(e.target.value)} />
+                                <button className="btn btn-success" onClick={createNewUser} >
+                                    Create User
+                                </button>
+                            </div>
+                            <h5>User List: </h5>
+                            <ul className="list-group">
+                                {Array.isArray(users) ? (
+                                    users.map((user, index) => (
+                                        <UserName key={index} index={index} name={user.name} />
+                                    ))) : (<li>No users found</li>)}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row justify-content-center mt-2">
                 <div className="col-md-6">
                     <div className="card shadow">
                         <div className="card-body">
